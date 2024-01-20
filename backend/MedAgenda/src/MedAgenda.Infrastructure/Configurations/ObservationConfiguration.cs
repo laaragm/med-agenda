0040de75467fcf4bcaa84cf34719c2c3ea926cb6
Lara <laragalvanim@gmail.com>
2024-01-20 17:59:50 -0300

    Add EF Core configurations

---
+++ backend/MedAgenda/src/MedAgenda.Infrastructure/Configurations/ObservationConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using MedAgenda.Domain.Observations;
+using MedAgenda.Domain.Patients;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MedAgenda.Infrastructure.Configurations;
+
+internal sealed class ObservationConfiguration : IEntityTypeConfiguration<Observation>
+{
+	public void Configure(EntityTypeBuilder<Observation> builder)
+	{
+		builder.ToTable("observations");
+
+		builder.HasKey(observation => observation.Id);
+
+		// Define a conversion from the strongly typed id into the Guid value and also from the database value into the strongly typed id
+		builder.Property(observation => observation.Id)
+			.HasConversion(observation => observation.Value, value => new ObservationId(value));
+
+		builder.Property(observation => observation.Message)
+			.HasConversion(name => name.Value, value => new Message(value));
+
+		builder.Property(observation => observation.CreatedBy);
+
+		builder.Property(observation => observation.CreatedOn);
+
+		builder.Property(observation => observation.UpdatedBy);
+
+		builder.Property(observation => observation.UpdatedOn);
+
+		builder.HasOne<Patient>() // An observation has one patient associated with it
+			.WithMany() // A patient can have many observations
+			.HasForeignKey(observation => observation.PatientId);
+
+		// Define a shadow property and instruct EF Core to interpret this column as a row version for implementing optimistic concurrency support
+		builder.Property<uint>("Version").IsRowVersion();
+	}
+}
--- backend/MedAgenda/src/MedAgenda.Infrastructure/Configurations/PatientConfiguration.cs
+++ backend/MedAgenda/src/MedAgenda.Infrastructure/Configurations/PatientConfiguration.cs
@@ -24,8 +24,25 @@ internal sealed class PatientConfiguration : IEntityTypeConfiguration<Patient>
 			.HasMaxLength(20)
 			.HasConversion(phoneNumber => phoneNumber != null ? phoneNumber.Value : null, value => value != null ? new PhoneNumber(value) : null);
 
-		//builder.Property(patient => patient.MedicalState)
-		//	.HasConversion(medicalState => medicalState, value => new PhoneNumber(value));
+		builder.Property(patient => patient.MedicalState)
+			.HasConversion(medicalState => medicalState.Code, value => MedicalState.GetByCode(value));
+
+		builder
+			.HasOne<Patient>() // A patient references another patient
+			.WithMany() // Referenced patient can be referenced by multiple patients
+			.HasForeignKey(p => p.ReferencePatientId);
+
+		builder.Property(patient => patient.IsTermSigned)
+			.HasConversion(isTermSigned => isTermSigned.Value, value => new IsTermSigned(value));
+
+		builder.Property(patient => patient.PeriodicityInDays)
+			.HasConversion(periodicity => periodicity != null ? periodicity.Value : (int?)null, value => value != null ? new PeriodicityInDays(value.Value) : null);
+
+		builder.Property(patient => patient.CreatedOn);
+
+		builder.Property(patient => patient.UpdatedBy);
+
+		builder.Property(patient => patient.UpdatedOn);
 
 		// Define a shadow property and instruct EF Core to interpret this column as a row version for implementing optimistic concurrency support
 		builder.Property<uint>("Version").IsRowVersion();
