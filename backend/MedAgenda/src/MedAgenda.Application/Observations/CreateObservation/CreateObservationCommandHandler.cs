fd2d781ed1973b42eafc51a74eb0e08c0ffe2aa4
Lara <laragalvanim@gmail.com>
2024-01-28 11:32:37 -0300

    Adjust create observation command handler

--- backend/MedAgenda/src/MedAgenda.Application/Observations/CreateObservation/CreateObservationCommandHandler.cs
+++ backend/MedAgenda/src/MedAgenda.Application/Observations/CreateObservation/CreateObservationCommandHandler.cs
@@ -11,17 +11,31 @@ internal class CreateObservationCommandHandler : ICommandHandler<CreateObservati
 	private readonly IUnitOfWork _unitOfWork;
 	private readonly IDateTimeProvider _dateTimeProvider;
 	private readonly IObservationRepository _observationRepository;
+	private readonly IPatientRepository _patientRepository;
 
-	public CreateObservationCommandHandler(IUnitOfWork unitOfWork, IDateTimeProvider dateTimeProvider, IObservationRepository observationRepository)
+	public CreateObservationCommandHandler(
+		IUnitOfWork unitOfWork, 
+		IDateTimeProvider dateTimeProvider, 
+		IObservationRepository observationRepository,
+		IPatientRepository patientRepository)
 	{
 		_unitOfWork = unitOfWork;
 		_dateTimeProvider = dateTimeProvider;
 		_observationRepository = observationRepository;
+		_patientRepository = patientRepository;
 	}
 
 	public async Task<Result<Guid>> Handle(CreateObservationCommand request, CancellationToken cancellationToken)
 	{
-		var observation = Observation.Create(new PatientId(request.PatientId), new Message(request.Message), _dateTimeProvider.UtcNow, request.CreatedBy);
+		var observation = Observation.Create(
+			new PatientId(request.PatientId), 
+			new Message(request.Message), 
+			_dateTimeProvider.UtcNow, 
+			request.CreatedBy);
+
+		var patient = await _patientRepository.GetByIdAsync(new PatientId(request.PatientId), cancellationToken);
+		if (patient is null)
+			return Result.Failure<Guid>(PatientErrors.NotFound);
 
 		_observationRepository.Add(observation);
 		await _unitOfWork.SaveChangesAsync(cancellationToken);
