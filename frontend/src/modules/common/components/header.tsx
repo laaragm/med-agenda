74c9ff2522d19d3748d3072b7d51d2d7c5f9eacb
Lara <laragalvanim@gmail.com>
2024-02-17 17:18:11 -0300

    Implement display of user's profile picture with fallback to initials in header

--- frontend/src/modules/common/components/header.tsx
+++ frontend/src/modules/common/components/header.tsx
@@ -1,21 +1,24 @@
 import LogoImg from "@/assets/logo.svg";
-import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
-import { Popover, PopoverContent, PopoverTrigger } from "./popover";
+import { signOut } from "@/authentication";
+import { useAuth, useProfilePicture } from "@/common/hooks";
+import { Avatar, AvatarFallback, AvatarImage, Popover, PopoverContent, PopoverTrigger } from "@/common/components";
 
 export function Header() {
+	const { user } = useAuth();
+	const { data } = useProfilePicture();
+
 	return (
 		<div className="h-[55px] w-full flex items-end justify-between px-5">
 			<img src={LogoImg} alt="MedAgenda Logo" height={30} width={200} />
 
-			{/* TODO: Fetch from Graph and sign out */}
 			<Popover>
 				<PopoverTrigger>
 					<Avatar>
-						<AvatarImage src="https://github.com/laaragm.png" />
-						<AvatarFallback>LG</AvatarFallback>
+						<AvatarImage src={data} />
+						<AvatarFallback className="bg-primary text-white font-semibold">{user?.initials}</AvatarFallback>
 					</Avatar>
 				</PopoverTrigger>
-				<PopoverContent>Sair</PopoverContent>
+				<PopoverContent onClick={signOut}>Sair</PopoverContent>
 			</Popover>
 		</div>
 	);
